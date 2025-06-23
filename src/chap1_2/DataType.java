3be04d89b44ddf640a9f41b3b805a1625cf03671
hsp64 <pphs604@gmail.com>
2025-06-23 09:52:07 +0900

    dataType

---
+++ src/chap1_2/DataType.java
@@ -0,0 +1,16 @@
+package chap1_2;
+
+public class DataType {
+
+    public static void main(String[] args) {
+
+        // 정적 타이핑 언어
+        int age = 20;
+        double pi = 3.14;
+        char c = 'A';
+        String greeting = "안녕하세요";
+        boolean flag = true;
+
+
+    }
+}
