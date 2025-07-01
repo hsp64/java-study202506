2c38dd990317ef240ce8c71c6f6239b5fa00580b
hsp64 <pphs604@gmail.com>
2025-07-01 10:52:47 +0900

    메서드 오버로딩 및 상속 예제 구현

--- src/chap1_7/inherit/player/Main.java
+++ src/chap1_7/inherit/player/Main.java
@@ -1,5 +1,17 @@
 package chap1_7.inherit.player;
 
+/*
+    Object 는 모든 클래스의 부모이다.
+                  Object
+                    |
+                 Player
+             /     |     \
+         Warrior  Mage   Hunter
+           /    \  |
+     Berserker  warMage
+     기본 상속에서는 다중상속을 막고 있다.
+ */
+
 public class Main {
     public static void main(String[] args) {
 
@@ -9,6 +21,8 @@ public class Main {
 
         warrior.showStatus();
         warrior.attack();
+
+//        warrior.
         System.out.println("==========");
 
         mage.showStatus();
--- src/chap1_7/inherit/player/Warrior.java
+++ src/chap1_7/inherit/player/Warrior.java
@@ -1,14 +1,28 @@
 package chap1_7.inherit.player;
 
+
 // 부모로부터 기능과 속성을 물려받는 자식 클래스 (sub class)
 // 상속은 IS A 관계 - 사자는 동물이다.  학생은 인간이다.   소나타는 자동차다.
 public class Warrior extends Player{
 
     int rage; // 분노게이지
 
+    public Warrior() {
+        super();
+    }
+
     public Warrior(String nickName) {
         super(nickName,100); // 부모 생략가능하며 원래는 super.~~ 지만 this도 허용한다.
         this.rage = 0;
+    }
+
+    // 오버라이딩: 부모가 물려준 메서드를 고쳐 쓰는 것
+    // 규칙: 1.부모의 시그니처(이름, 리턴타입, 파라미터)와 동일해야 함
+    //      2. 접근제어자는 more public
+    @Override // 오버라이딩 룰을 위반했는지 체크
+    public void showStatus() {
+        super.showStatus();
+        System.out.println("# 분노: " + this.rage);
     }
 
     void dash() {
