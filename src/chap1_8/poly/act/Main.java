9723b537e4e7b67e0a322a41aae27f26632473e7
hsp64 <pphs604@gmail.com>
2025-07-02 09:55:03 +0900

    다형성 예제 클래스 및 메서드 구현

---
+++ src/chap1_8/poly/act/Main.java
@@ -0,0 +1,9 @@
+package chap1_8.poly.act;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Actor[] actors = new Actor[] {};
+    }
+}
---
+++ src/chap1_8/poly/dirve/CarShop.java
@@ -0,0 +1,5 @@
+package chap1_8.poly.dirve;
+
+public class CarShop {
+
+}
