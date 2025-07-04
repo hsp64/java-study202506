f6bced7599a30f16b4fe4fd8424b06cf2bd97728
hsp64 <pphs604@gmail.com>
2025-07-04 11:02:52 +0900

    날짜와 시간, 문자열 처리, 래퍼 클래스 활용 예제 추가, DateTime/StringExample/WrapperExample 클래스 작성 및 관련 테스트 로직 구현

---
+++ src/chap2_2/lang/time/DateTime.java
@@ -0,0 +1,24 @@
+package chap2_2.lang.time;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class DateTime {
+
+    public static void main(String[] args) {
+
+        // 날짜와 시간을 표현
+        // 1970년 1월 1일 이전의 시간을 모름 - 에폭 타임 (유닉스 타임)
+        // 밀리초단위로 시간을 늘려나감
+        System.out.println(System.currentTimeMillis());
+
+        // 자바 8 이전
+        Date date = new Date();
+        System.out.println(date);
+
+        Calendar calendar = Calendar.getInstance();
+//        System.out.println(calendar.JULY);
+
+    }
+
+}
