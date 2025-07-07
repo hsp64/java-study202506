a01153f3b9d5718d8993c0d8d9e9dda034d676ec
hsp64 <pphs604@gmail.com>
2025-07-07 10:50:47 +0900

    map과  set

---
+++ src/chap2_4/collection/song/ArtistRepository.java
@@ -0,0 +1,13 @@
+package chap2_4.collection.song;
+
+import java.util.Map;
+
+// 가수 객체를 자료구조로 관리하는 클래스
+public class ArtistRepository {
+
+    // 가수들을 담을 자료구조
+    // key: 가수명, value: 가수 객체
+    private Map<String, Artist> artistMap;
+
+
+}
