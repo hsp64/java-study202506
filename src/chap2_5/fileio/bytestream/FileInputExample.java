2c18b22c08b1f182de086cd278eebfababa60189
hsp64 <pphs604@gmail.com>
2025-07-08 09:55:21 +0900

    입출력 기초

---
+++ src/chap2_5/fileio/bytestream/FileInputExample.java
@@ -0,0 +1,26 @@
+package chap2_5.fileio.bytestream;
+
+import chap2_5.fileio.FileExample;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class FileInputExample {
+
+    public static void main(String[] args) {
+
+        try {
+            FileInputStream fis = new FileInputStream(FileExample.ROOT_PATH + "/pet.txt");
+
+            int data = 0;
+            while ((data = fis.read()) != -1) {
+//                System.out.print((char) data);
+                System.out.write(data); // 숫자를 아스키로 변환해서 출력
+            }
+            // 출력 버퍼 비우기
+            System.out.flush();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
